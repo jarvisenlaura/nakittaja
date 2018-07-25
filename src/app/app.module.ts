68b0ae6c88fc7a1795dfba91df1dfed90b658b37
Laura Järvinen <laura@iwa.fi>
2018-07-25 15:03:32 +0300

    Added a planner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlannerComponent } from './components/planner/planner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlannerComponent
   ],
   imports: [
     BrowserModule

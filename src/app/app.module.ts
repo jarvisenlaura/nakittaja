d35df2f64db90ad18f5b35a819b8596f6bb0b43c
Laura Järvinen <laura@iwa.fi>
2018-07-26 10:56:43 +0300

    FormsModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PlannerComponent } from './components/planner/planner.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PlannerComponent } from './components/planner/planner.component';
     PlannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

0327fd89b763bc2fd60837b3b35bdb2a3f41c0da
Laura Järvinen <laura@iwa.fi>
2018-07-30 14:42:09 +0300

    Added Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlannerComponent } from './components/planner/planner.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PlannerComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

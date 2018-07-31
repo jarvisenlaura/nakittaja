5b76ab625c0dc4b9f650b9474e723482049c5286
Laura Järvinen <laura@iwa.fi>
2018-07-31 22:55:16 +0300

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { AppComponent } from './app.component';
 import { PlannerComponent } from './components/planner/planner.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { RouterModule, Routes } from '../../node_modules/@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'planner', component: PlannerComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +21,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
